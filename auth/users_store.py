984368c51ddd67e908b34e7e0623cad84c06ade4
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 08:51:33 -0300

    Create users_store.py

---
+++ auth/users_store.py
@@ -0,0 +1,19 @@
+# auth/users_store.py
+
+USERS = {
+    "admin": {
+        "password": "admin123",
+        "role": "ADMIN",
+        "active": True
+    },
+    "secretaria1": {
+        "password": "secre123",
+        "role": "SECRETARIA",
+        "active": True
+    },
+    "medico1": {
+        "password": "medico123",
+        "role": "MEDICO",
+        "active": True
+    }
+}
