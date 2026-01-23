676b29d64dbb9ff17843719130f7f8207bf0b9d0
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-22 23:10:03 -0300

    Delete components directory

--- components/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, children }) {
-  return (
-    <div className="card">
-      <h3>{title}</h3>
-      {children}
-    </div>
-  );
-}
--- components/TopBar.jsx
+++
@@ -1,8 +0,0 @@
-export default function TopBar({ role }) {
-  return (
-    <div className="topbar">
-      <strong>Ficha Clínica</strong>
-      <span className="role">{role}</span>
-    </div>
-  );
-}
