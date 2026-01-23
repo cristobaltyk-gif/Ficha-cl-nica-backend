cb659346dc192ddf8a08b58901903263ca3151f2
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 08:50:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from auth.auth_service import login_router
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app = FastAPI(
+    title="Ficha Clínica – Backend",
+    version="0.1"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # luego se restringe
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 🔐 Auth
+app.include_router(login_router)
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
