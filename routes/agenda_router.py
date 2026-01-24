36c5d74a5592005663d07ea9851595a79facfa44
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-24 19:48:25 -0300

    Delete routes/agenda_router.py

--- routes/agenda_router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Query
-from services.agenda_service import build_agenda
-
-agenda_router = APIRouter()
-
-@agenda_router.get("/agenda")
-def get_agenda(date: str = Query(...)):
-    return build_agenda(date)
