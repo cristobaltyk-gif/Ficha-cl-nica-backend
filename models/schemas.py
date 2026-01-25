92d35c53171e4f4a7352d01c3739b4d0c2b73c75
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-25 17:37:06 -0300

    Update schemas.py

--- models/schemas.py
+++ models/schemas.py
@@ -1,9 +1,25 @@
 from pydantic import BaseModel
+from typing import List
+
+# ==========================
+# REQUEST
+# ==========================
 
 class LoginRequest(BaseModel):
     usuario: str
     clave: str
 
+
+# ==========================
+# RESPONSE
+# ==========================
+
+class RoleSchema(BaseModel):
+    name: str           # "SECRETARIA", "MEDICO", "ADMIN"
+    entry: str          # "/secretaria", "/medico", etc.
+    allow: List[str]    # ["agenda", "pacientes", ...]
+
+
 class LoginResponse(BaseModel):
     usuario: str
-    role: str
+    role: RoleSchema
