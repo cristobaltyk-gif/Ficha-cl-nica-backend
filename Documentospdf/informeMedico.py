71e19ee62992e6b5a2ba2b4250c9fce242860f12
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-02-26 16:29:35 -0300

    Update informeMedico.py

--- Documentospdf/informeMedico.py
+++ Documentospdf/informeMedico.py
@@ -19,7 +19,7 @@ def calcular_edad(fecha_nacimiento_str):
         return ""
 
 
-def generarRecetaMedica(buffer, datos):
+def generar_informe_pdf(buffer, datos):
 
     # =========================
     # PACIENTE
